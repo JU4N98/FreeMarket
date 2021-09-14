b954bbe7863787e95f63e6dd7bdce5ec704d1468
juan <juanpablocabana2@gmail.com>
2021-09-14 16:55:25 -0300

    Changes style

--- app/src/main/java/com/example/freemarket/MainActivity.java
+++ app/src/main/java/com/example/freemarket/MainActivity.java
@@ -6,15 +6,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
-import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.regex.Pattern;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity{
         CheckBox checkBoxRetiro = findViewById(R.id.checkBoxRetiro);
         Switch switchDescuento = findViewById(R.id.switchDescuento);
         SeekBar seekBarDescuento = findViewById(R.id.seekBarDescuento);
+        TextView textViewPorcentaje = findViewById(R.id.textViewPorcentaje);
         TextView textViewDireccionRetiro = findViewById(R.id.textViewDireccion);
         EditText editTextDireccionRetiro = findViewById((R.id.editTextDireccion));
         CheckBox checkBoxTyC = findViewById(R.id.checkBoxTyC);
@@ -34,33 +35,55 @@ public class MainActivity extends AppCompatActivity{
 
         editTextDireccionRetiro.setVisibility(View.GONE);
         textViewDireccionRetiro.setVisibility(View.GONE);
-        checkBoxRetiro.setOnClickListener(e->{
-            if(checkBoxRetiro.isChecked()){
+        checkBoxRetiro.setOnClickListener(e -> {
+            if (checkBoxRetiro.isChecked()) {
                 editTextDireccionRetiro.setVisibility(View.VISIBLE);
                 textViewDireccionRetiro.setVisibility(View.VISIBLE);
-            }else{
+            } else {
                 editTextDireccionRetiro.setVisibility(View.GONE);
                 textViewDireccionRetiro.setVisibility(View.GONE);
             }
         });
 
         seekBarDescuento.setVisibility(View.GONE);
-        switchDescuento.setOnClickListener(e->{
-            if(switchDescuento.isChecked()) seekBarDescuento.setVisibility(View.VISIBLE);
-            else seekBarDescuento.setVisibility(View.GONE);
+        textViewPorcentaje.setVisibility(View.GONE);
+        switchDescuento.setOnClickListener(e -> {
+            if (switchDescuento.isChecked()) {
+                seekBarDescuento.setVisibility(View.VISIBLE);
+                textViewPorcentaje.setVisibility(View.VISIBLE);
+            } else {
+                seekBarDescuento.setVisibility(View.GONE);
+                textViewPorcentaje.setVisibility(View.GONE);
+            }
+        });
+
+        textViewPorcentaje.setText(seekBarDescuento.getProgress() + "%");
+        seekBarDescuento.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textViewPorcentaje.setText(seekBarDescuento.getProgress() + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
         });
 
         buttonPublicar.setEnabled(false);
-        checkBoxTyC.setOnClickListener(e->{
-            if(checkBoxTyC.isChecked()) buttonPublicar.setEnabled(true);
+        checkBoxTyC.setOnClickListener(e -> {
+            if (checkBoxTyC.isChecked()) buttonPublicar.setEnabled(true);
             else buttonPublicar.setEnabled(false);
         });
 
 
-        buttonPublicar.setOnClickListener(e->{
+        buttonPublicar.setOnClickListener(e -> {
             int duration = Toast.LENGTH_SHORT;
             Toast toast = Toast.makeText(this, validarCampos(editTextDireccionRetiro, editTextTitulo, editTextPrecio, checkBoxRetiro,
-            switchDescuento, seekBarDescuento, editTextEmail), duration);
+                    switchDescuento, seekBarDescuento, editTextEmail), duration);
             toast.show();
         });
 
@@ -69,34 +92,32 @@ public class MainActivity extends AppCompatActivity{
 
 
     private CharSequence validarCampos(EditText retiro, EditText titulo, EditText precio, CheckBox retiroCheck,
-                                        Switch switchDescuento, SeekBar seekDescuento, EditText email){
+                                       Switch switchDescuento, SeekBar seekDescuento, EditText email) {
         // Regex
         Pattern patternEmail = Pattern.compile("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$");
         Pattern patternCampos = Pattern.compile("^[a-zA-Z0-9.,\n ]*$");
 
 
         // Campos obligatorios
-        if(titulo.getText().length() ==0 || precio.getText().length() == 0 || (retiroCheck.isChecked() && retiro.getText().length() == 0))
+        if (titulo.getText().length() == 0 || precio.getText().length() == 0 || (retiroCheck.isChecked() && retiro.getText().length() == 0))
             return "ERROR: Debe completar los campos marcados con *";
 
         // Descuento mayor a 0
-        if(switchDescuento.isChecked() && seekDescuento.getProgress()<=0)
+        if (switchDescuento.isChecked() && seekDescuento.getProgress() <= 0)
             return "ERROR: No puede ofrecer un descuento de 0%, suba el descuento o desmarque la opcion";
 
         // Precio mayor a 0
-        if(Float.parseFloat(String.valueOf(precio.getText())) <= 0)
+        if (Float.parseFloat(String.valueOf(precio.getText())) <= 0)
             return "ERROR: El precio debe ser mayor a cero";
 
         // Email con Regex
-        if(!patternEmail.matcher(email.getText()).matches())
+        if (!patternEmail.matcher(email.getText()).matches())
             return "ERROR: El email ingresado no es valido";
 
         // Campos Regex
-        if(!patternCampos.matcher(titulo.getText()).matches() || !patternCampos.matcher(precio.getText()).matches()
-            || (retiroCheck.isChecked() && !patternCampos.matcher(retiro.getText()).matches()))
+        if (!patternCampos.matcher(titulo.getText()).matches() || !patternCampos.matcher(precio.getText()).matches()
+                || (retiroCheck.isChecked() && !patternCampos.matcher(retiro.getText()).matches()))
             return "ERROR: Uno o mas campos invalidos. Verifique que solo contengan letras, numeros, puntos, comas o saltos de linea";
-
-
 
 
         return "Éxito";
